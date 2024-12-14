9e66f660a79edd5eb707f64bfb5fca6c9c6e8a4c
Dmitriy Ivanov <dmitriy.ivanov@travelline.ru>
2024-12-14 19:50:04 +0300

    feat: Обновил иконки

--- game/Assets/Scripts/Application/Menus/MainMenus/MainMenuUi.cs
+++ game/Assets/Scripts/Application/Menus/MainMenus/MainMenuUi.cs
@@ -7,7 +7,8 @@ namespace Assets.Scripts.Application.Menus.MainMenus
     internal class MainMenuUi
     {
         private readonly GameObject _gameObject;
-        
+
+        public readonly EventManager SoundButtonEventManager = new();
         public readonly EventManager ArcadeButtonEventManager = new();
         public readonly EventManager SandboxButtonEventManager = new();
         public readonly EventManager TreeButtonEventManager = new();
@@ -15,13 +16,15 @@ namespace Assets.Scripts.Application.Menus.MainMenus
 
         public MainMenuUi( GameObject gameObject )
         {
-            _gameObject = gameObject;
+            _gameObject = gameObject.ThrowIfNull( nameof( gameObject ) );
+            var mainMenuChildrenContainer = new GameObjectChildrenContainer( gameObject );
+            mainMenuChildrenContainer.Get( "sound_button" ).GetComponent<Button>().onClick.AddListener( SoundButtonEventManager.Trigger );
             
-            var childrenContainer = new GameObjectChildrenContainer( gameObject );
-            childrenContainer.Get( "arcade_button" ).GetComponent<Button>().onClick.AddListener( ArcadeButtonEventManager.Trigger );
-            childrenContainer.Get( "sandbox_button" ).GetComponent<Button>().onClick.AddListener( SandboxButtonEventManager.Trigger );
-            childrenContainer.Get( "tree_button" ).GetComponent<Button>().onClick.AddListener( TreeButtonEventManager.Trigger );
-            childrenContainer.Get( "exit_button" ).GetComponent<Button>().onClick.AddListener( ExitButtonEventManager.Trigger );
+            var mainListChildrenContainer = new GameObjectChildrenContainer( mainMenuChildrenContainer.Get( "main_list" ) );
+            mainListChildrenContainer.Get( "arcade_button" ).GetComponent<Button>().onClick.AddListener( ArcadeButtonEventManager.Trigger );
+            mainListChildrenContainer.Get( "sandbox_button" ).GetComponent<Button>().onClick.AddListener( SandboxButtonEventManager.Trigger );
+            mainListChildrenContainer.Get( "tree_button" ).GetComponent<Button>().onClick.AddListener( TreeButtonEventManager.Trigger );
+            mainListChildrenContainer.Get( "exit_button" ).GetComponent<Button>().onClick.AddListener( ExitButtonEventManager.Trigger );
         }
 
         public void SetActive( bool activity ) => _gameObject.SetActive( activity );
